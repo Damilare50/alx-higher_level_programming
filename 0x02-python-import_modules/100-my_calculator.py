4b46826b0d8f8a6a7eaa419f1390805dadfb5ab1
Damilare50 <damilarelamine@gmail.com>
2022-06-02 16:10:00 +0100

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
-import calculator_1
 if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
     lent = len(sys.argv)
     if lent != 4:
         print("Usage: {} <a> <operator> <b>".format(sys.argv[0])
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     num1 = int(sys.argv[1])
     num2 = int(sys.argv[3])
     if oper == '+':
-        print("{} + {} = {}".format(num1, num2, calculator_1.add(num1, num2)))
+        print("{} + {} = {}".format(num1, num2, add(num1, num2)))
     elif oper == '-':
-        print("{} - {} = {}".format(num1, num2, calculator_1.sub(num1, num2)))
+        print("{} - {} = {}".format(num1, num2, sub(num1, num2)))
     elif oper == '*':
-        print("{} * {} = {}".format(num1, num2, calculator_1.mul(num1, num2)))
+        print("{} * {} = {}".format(num1, num2, mul(num1, num2)))
     elif oper == '/':
-        print("{} / {} = {}".format(num1, num2, calculator_1.div(num1, num2)))
+        print("{} / {} = {}".format(num1, num2, div(num1, num2)))
     else:
         print("Unknown Operator. Available operators: +, -, * and /")
         exit(1)
